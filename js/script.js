e500b91f599a49426f9f74d0aa60a2868186cab9
ChelGame <53932772+ChelGame@users.noreply.github.com>
2020-06-25 15:23:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+  $('#button').click(function () {
+    $('.body').toggleClass('un__scroll');
+    $('.n__menu').toggleClass('visually-hidden');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+// Всякое разное по работе
+//
+//
+//
+//
